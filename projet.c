c515395a6cf25916fcf616f11664802f56309d3c
Terrabytz <44778637+Terrabytz@users.noreply.github.com>
2018-11-05 18:24:52 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -4,35 +4,50 @@
 
 //DECLARATION DES VARIABLES GLOBALES
 
-int grille_inv[10][8];
-
+int grille[4][8];
+int grille_plateau[64][128];
+int deplacement_possible=0;
 //DECLARATION DES STRUCTURES
-typedef struct joueur
-{
-
-}
+typedef struct joueur{
+    int touche;
+};
+typedef struct invader{
+    int pv;
+    int positioncentrale;
+    int numero_d_identification;
+};
 
 
 //DECLARATION DES FONCTIONS
-void aff_invader()
-void init_inv(*grille[10][8])
-{
-    *grille[0][0]=0; *grille[0][1]=0; *grille[0][2]=0; *grille[0][3]=0; *grille[0][4]=0; *grille[0][5]=0; *grille[0][6]=0; *grille[0][7]=0;
-    *grille[1][0]=0; *grille[1][1]=0; *grille[1][2]=0; *grille[1][3]=0; *grille[1][4]=0; *grille[1][5]=0; *grille[1][6]=0; *grille[1][7]=0;
-    *grille[2][0]=0; *grille[2][1]=0; *grille[2][2]=0; *grille[2][3]=0; *grille[2][4]=0; *grille[2][5]=0; *grille[2][6]=0; *grille[2][7]=0;
-    *grille[3][0]=0; *grille[3][1]=0; *grille[3][2]=0; *grille[3][3]=0; *grille[3][4]=0; *grille[3][5]=0; *grille[3][6]=0; *grille[3][7]=0;
-    *grille[4][0]=0; *grille[4][1]=0; *grille[4][2]=0; *grille[4][3]=0; *grille[4][4]=0; *grille[4][5]=0; *grille[4][6]=0; *grille[4][7]=0;
-    *grille[5][0]=0; *grille[5][1]=0; *grille[5][2]=0; *grille[5][3]=0; *grille[5][4]=0; *grille[5][5]=0; *grille[5][6]=0; *grille[5][7]=0;
-    *grille[6][0]=0; *grille[6][1]=0; *grille[6][2]=0; *grille[6][3]=0; *grille[6][4]=0; *grille[6][5]=0; *grille[6][6]=0; *grille[6][7]=0;
-    *grille[7][0]=0; *grille[7][1]=0; *grille[7][2]=0; *grille[7][3]=0; *grille[7][4]=0; *grille[7][5]=0; *grille[7][6]=0; *grille[7][7]=0;
-    *grille[8][0]=0; *grille[8][1]=0; *grille[8][2]=0; *grille[8][3]=0; *grille[8][4]=0; *grille[8][5]=0; *grille[8][6]=0; *grille[8][7]=0;
-    *grille[9][0]=0; *grille[9][1]=0; *grille[9][2]=0; *grille[9][3]=0; *grille[9][4]=0; *grille[9][5]=0; *grille[9][6]=0; *grille[9][7]=0;
-
-void dep_inv()
-
-
-
-int main()
-{
-
+void aff_invader(){ //imprime un invader dans la grille_plateau avec son numéro
+    
+}
+void cleanse( grille_plateau[64][128]){ //vide grille_plateau de toutes valeurs
+    int i,j = 0;
+    for (i<64;i=0;i++){
+        for (j<128;j=0;j++){
+            grille_plateau[i][j] = 0;
+        }
+    }
+}
+            
+   
+void init_inv(grille=[4][8]){ //initialise le premier niveau
+    grille[0][0]=0; grille[0][1]=0; grille[0][2]=0; grille[0][3]=0; grille[0][4]=0; grille[0][5]=0; grille[0][6]=0; grille[0][7]=0;
+    grille[1][0]=0; grille[1][1]=0; grille[1][2]=0; grille[1][3]=0; grille[1][4]=0; grille[1][5]=0; grille[1][6]=0; grille[1][7]=0;
+    grille[2][0]=0; grille[2][1]=0; grille[2][2]=0; grille[2][3]=0; grille[2][4]=0; grille[2][5]=0; grille[2][6]=0; grille[2][7]=0;
+    grille[3][0]=0; grille[3][1]=0; grille[3][2]=0; grille[3][3]=0; grille[3][4]=0; grille[3][5]=0; grille[3][6]=0; grille[3][7]=0;
+}
+void affichage(grille_plateau[64][128]){ //fonction qui fait l'affichage de l'écran glcd
+void deplacement_horizontal_inv(){ 
+     while(deplacement_possible==1){
+          if(1){
+              
+          }
+     }
+}
+int deplacementtest(grille_plateau[64][128]){
+     for(int numero_invader_test=1;numero_invader_test<=40;numero_invader_test++){
+         
+     }
 }
