2dff00470dd1203ecb4f20bb676661e9484e0295
Terrabytz <44778637+Terrabytz@users.noreply.github.com>
2018-11-05 16:36:06 +0100

    space_debut
    debut du projet

---
+++ projet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//DECLARATION DES VARIABLES GLOBALES
+
+int grille_inv[10][8];
+
+//DECLARATION DES STRUCTURES
+typedef struct joueur
+{
+
+}
+
+
+//DECLARATION DES FONCTIONS
+void aff_invader()
+void init_inv(*grille[10][8])
+{
+    *grille[0][0]=0; *grille[0][1]=0; *grille[0][2]=0; *grille[0][3]=0; *grille[0][4]=0; *grille[0][5]=0; *grille[0][6]=0; *grille[0][7]=0;
+    *grille[1][0]=0; *grille[1][1]=0; *grille[1][2]=0; *grille[1][3]=0; *grille[1][4]=0; *grille[1][5]=0; *grille[1][6]=0; *grille[1][7]=0;
+    *grille[2][0]=0; *grille[2][1]=0; *grille[2][2]=0; *grille[2][3]=0; *grille[2][4]=0; *grille[2][5]=0; *grille[2][6]=0; *grille[2][7]=0;
+    *grille[3][0]=0; *grille[3][1]=0; *grille[3][2]=0; *grille[3][3]=0; *grille[3][4]=0; *grille[3][5]=0; *grille[3][6]=0; *grille[3][7]=0;
+    *grille[4][0]=0; *grille[4][1]=0; *grille[4][2]=0; *grille[4][3]=0; *grille[4][4]=0; *grille[4][5]=0; *grille[4][6]=0; *grille[4][7]=0;
+    *grille[5][0]=0; *grille[5][1]=0; *grille[5][2]=0; *grille[5][3]=0; *grille[5][4]=0; *grille[5][5]=0; *grille[5][6]=0; *grille[5][7]=0;
+    *grille[6][0]=0; *grille[6][1]=0; *grille[6][2]=0; *grille[6][3]=0; *grille[6][4]=0; *grille[6][5]=0; *grille[6][6]=0; *grille[6][7]=0;
+    *grille[7][0]=0; *grille[7][1]=0; *grille[7][2]=0; *grille[7][3]=0; *grille[7][4]=0; *grille[7][5]=0; *grille[7][6]=0; *grille[7][7]=0;
+    *grille[8][0]=0; *grille[8][1]=0; *grille[8][2]=0; *grille[8][3]=0; *grille[8][4]=0; *grille[8][5]=0; *grille[8][6]=0; *grille[8][7]=0;
+    *grille[9][0]=0; *grille[9][1]=0; *grille[9][2]=0; *grille[9][3]=0; *grille[9][4]=0; *grille[9][5]=0; *grille[9][6]=0; *grille[9][7]=0;
+
+void dep_inv()
+
+
+
+int main()
+{
+
+}
