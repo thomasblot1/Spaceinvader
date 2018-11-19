aee97ac568c3c4afe3ec707b15797f94cbe30cdf
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2018-11-19 15:18:46 +0100

    deplacement horizontal function

--- projet.c
+++ projet.c
@@ -52,11 +52,31 @@ void affichage(grille_plateau[64][128])//fonction qui fait l'affichage de l'écr
     } 
 }
 void deplacement_horizontal_inv(){ 
-     while(deplacement_possible==1){
-          if(1){
-              
+  
+  int i,j = 0;
+  char gauche='oui';
+  while(deplacement_possible==1){
+    if(gauche=='oui'){
+      for(i=0;i<64;i++){
+        for(y=0;y<128;y++){
+          if(grille_plateau[i][y]!=0){
+            grille_plateau[i][y+1]=grille_plateau[i][y];
+            grille_plateau[i][y]=0;
           }
+        }
+      }
+    }
+  else{
+         for(i=0;i<64;i++){
+             for(y=0;y<128;y++){
+                 if(grille_plateau[i][y]!=0){
+                     grille_plateau[i+1][y]=grille_plateau[i][y];
+                     grille_plateau[i][y]=0;
+                 }
+             }
+         }
      }
+   }
 }
 int deplacementtest(grille_plateau[64][128]){
      for(int numero_invader_test=1;numero_invader_test<=40;numero_invader_test++){
@@ -133,36 +153,3 @@ fire()
         wait ();
         
     }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    
