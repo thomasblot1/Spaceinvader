81d0e7cf6f364245fd710c995c6a1c1314870915
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2018-11-26 16:12:20 +0100

    [affichage.c] sprintf

--- affichage.c
+++ affichage.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "glcd.h"
 
 void convertisseur(grille_64_128[64][128], logo[8191]){
   int i,y,rang=0;
   for(i=0;i<64;i++){
     for(y=0;y<128;y++){
-      if(i%8==0)
+      if()
+      if((i%8==0)&&(i!=0)){
+        sprintf(num,"%x",compteur);
+        char c=0x+num;
+        logo[rang]=(char)c;
+        rang++;
+      }
+
+
     }
   }
 }
