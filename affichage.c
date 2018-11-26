e6f1445296995c21d22f5b50457369f430191b32
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2018-11-26 15:15:01 +0100

    [affichage] debut

--- affichage.c
+++ affichage.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "glcd.h"
+
+void convertisseur(grille_64_128[64][128], logo[8191]){
+  int i,y,rang=0;
+  for(i=0;i<64;i++){
+    for(y=0;y<128;y++){
+      if(i%8==0)
+    }
+  }
+}
