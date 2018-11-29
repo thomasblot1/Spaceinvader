92c0ddccbdaed22727c4de8ddde3e1a2982b8a52
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2018-11-29 17:11:36 +0100

    [affichage.c] fin de la fonction de conversion test

--- affichage.c
+++ affichage.c
@@ -3,29 +3,23 @@
 #include <string.h>
 #include "glcd.h"
 
-void convertisseur(int[64][128] grille_64_128,char[8191] logo){
+
+void convertisseur(int grille_64_128[64][128],char logo[8191]){
   int i,y,rang_initial=0;
-  char d=0x;
+  for(i=0;i<8191;i++){
+    logo[i]=0;
+  }
   for(i=0;i<64;i++){
     for(y=0;y<128;y++){
+      logo[y+rang_initial]+=(char) ((2**i)*grille_64_128[i][y]);
       if((i%8==0)&&(i!=0)){
-
-void convertisseur(grille_64_128[64][128], logo[8191]){
-  int i,y,rang_initial=0;
-  for(i=0;i<64;i++){
-    for(y=0;y<128;y++){
-
-      if((i%8==0)&&(i!=0)){
-
-        rang_initial;
-      }
-
         rang_initial+=128;
       }
-      logo[y]=(char)2^i
     }
+
   }
 }
+
 /*
 sprintf(num,"%x",compteur);
 char c=0x+num;
