8387d0c7facfa0da3a6340021c40778476c18b96
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2018-11-29 22:06:49 +0100

    y

--- invaderinit.h
+++ invaderinit.h
@@ -1,5 +1,6 @@
 #ifndef INVADERINIT_H_INCLUDED
 #define INVADERINIT_H_INCLUDED
+void init_inv(int grille=[4][8]);
+void aff_invader(int grille_plateau[64][128], grille[4][8]);
 
-void aff_invader(int grille_plateau[64][128],int grille[4][8]);
 #endif
